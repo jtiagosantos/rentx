cd47715560852f6191c13fde3995a73d1db985f7
jtiagosantos <tiagosan040@gmail.com>
2022-09-12 08:06:21 -0300

    chore: configure theme with fonts and colors

---
+++ src/styles/theme/fonts.ts
@@ -0,0 +1,7 @@
+export const fonts = {
+  primary_400: 'Inter_400Regular',
+  primary_500: 'Inter_500Medium',
+  secondary_400: 'Archivo_400Regular',
+  secondary_500: 'Archivo_500Medium',
+  secondary_600: 'Archivo_600SemiBold',
+};
