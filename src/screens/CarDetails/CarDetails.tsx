98d0bc567c51d91bdcfc9277a3b5f9d44e18a7d4
jtiagosantos <tiagosan040@gmail.com>
2022-09-16 18:50:36 -0300

    feat: integrate CarDetails screen

--- src/screens/CarDetails/CarDetails.tsx
+++ src/screens/CarDetails/CarDetails.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 //components
 import { BackButton } from '../../components/BackButton/BackButton';
@@ -8,26 +8,26 @@ import { ImageSlider } from '../../components/ImageSlider/ImageSlider';
 import { Accessory } from '../../components/Accessory/Accessory';
 import { Button } from '../../components/Button/Button';
 
-//assets
-import speedImage from '../../assets/speed.svg';
-import accelerationImage from '../../assets/acceleration.svg';
-import forceImage from '../../assets/force.svg';
-import gasolineImage from '../../assets/gasoline.svg';
-import exchangeImage from '../../assets/exchange.svg';
-import peopleImage from '../../assets/people.svg';
+//utils
+import { getAccessoryIcon } from '../../utils/getAccessoryIcon';
+
+//types
+import { Car } from '../../types/Car';
 
 //styles
 import * as S from './styles';
 
 export const CarDetails = () => {
   const navigation = useNavigation();
+  const route = useRoute();
+  const { car } = route.params as { car: Car };
 
   const handleChooseRentalPeriod = () => {
     navigation.navigate('Scheduling');
   }
 
   const navigationToHomeScreen = () => {
-    navigation.navigate('Home')
+    navigation.goBack();
   }
 
   return (
@@ -44,37 +44,34 @@ export const CarDetails = () => {
 
         <S.ImageSliderWrapper>
           <ImageSlider 
-            images={['https://www.webmotors.com.br/imagens/prod/348415/AUDI_RS5_2.9_V6_TFSI_GASOLINA_SPORTBACK_QUATTRO_STRONIC_34841516415001715.webp?s=fill&w=236&h=135&q=70&t=true']}
+            images={car.photos}
           />
         </S.ImageSliderWrapper>
 
         <S.Content>
           <S.Details>
             <S.Description>
-              <S.Brand>Lamborghini</S.Brand>
-              <S.Name>Huracan</S.Name>
+              <S.Brand>{car.brand}</S.Brand>
+              <S.Name>{car.name}</S.Name>
             </S.Description>
 
             <S.Rent>
-              <S.Period>Ao dia</S.Period>
-              <S.Price>R$ 580</S.Price>
+              <S.Period>{car.rent.period}</S.Period>
+              <S.Price>R$ {car.rent.price}</S.Price>
             </S.Rent>
           </S.Details>
 
           <S.Accessories>
-            <Accessory name='380Km/h' icon={speedImage} />
-            <Accessory name='3.2s' icon={accelerationImage} />
-            <Accessory name='800 HP' icon={forceImage} />
-            <Accessory name='Gasolina' icon={gasolineImage} />
-            <Accessory name='Auto' icon={exchangeImage} />
-            <Accessory name='2 pessoas' icon={peopleImage} />
+            {car.accessories.map((accessory) => (
+              <Accessory 
+                key={accessory.type}
+                name={accessory.name} 
+                icon={getAccessoryIcon(accessory.type)} 
+              />
+            ))}
           </S.Accessories>
 
-          <S.About>
-            Este é automóvel desportivo. Surgiu do lendário touro de lide 
-            indultado na praça Real Maestranza de Sevilla. É um belíssimo 
-            carro para quem gosta de acelerar.
-          </S.About>
+          <S.About>{car.about}</S.About>
         </S.Content>
 
         <S.Footer>
